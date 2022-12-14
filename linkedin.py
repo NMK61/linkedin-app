c88c134bb391857cbe6473ddc901eefcbad25f9f
NMK61 <120355822+NMK61@users.noreply.github.com>
2022-12-13 18:04:52 -0800

    Add files via upload

--- linkedin.py
+++ linkedin.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 #loading data file
-s = pd.read_csv("C:/Users/19496/Desktop/Module 2/social_media_usage.csv")
+s = pd.read_csv("C:C:\Users\19496\my_projectNK\social_media_usage.csv")
 
 
 #import, clean data
